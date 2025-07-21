1e489965ddc6e71e727dde9b9eb8a9b1c3d9f79b
XtremeVerse <namelessnameless799@gmail.com>
2025-07-21 21:21:35 +0530

    Delete src directory

--- src/hooks/useAnimationOnce.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useInView } from "framer-motion";
-
-export default function useAnimationOnce(ref) {
-  const isInView = useInView(ref, { once: true });
-  return isInView;
-}
