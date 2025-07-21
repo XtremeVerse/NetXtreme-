3e027815a889c13a55dad2ea02fc03b800b04cea
XtremeVerse <namelessnameless799@gmail.com>
2025-07-21 22:07:51 +0530

    Add files via upload

---
+++ _config.php
@@ -0,0 +1,6 @@
+<?php
+$websiteTitle = 'NetXtreme';
+$websiteUrl = 'https://netxtreme.vercel.app';
+$version = '1.0.0';
+$banner = '/public/logo/banner.png';
+?>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
+?>
+<!-- HiAnime style page will load here -->
